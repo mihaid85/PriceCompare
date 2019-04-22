7ef3be322c9c033df28b8bf8d09a9c85d99d3f6c
Danet Mihai <mihaid_85@yahoo.com>
2019-04-22 09:40:48 +0300

    ...

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,103 +4,99 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
 
-  data;
-  csvContent: string;
-  parsedCsv: string[][];
+    data;
+    csvContent: string;
+    parsedCsv: string[][];
+    product: {};
+    productsCollection = {};
 
-  constructor(private db: AngularFirestore, private http: HttpClient) { }
+    constructor(private db: AngularFirestore, private http: HttpClient) { }
 
-  ngOnInit() {
+    ngOnInit() {
+        let str = "//afftrk.altex.ro/Counter/Click?adsx=y4UWZE+abj3dU692c+4sFg==&rd=https%3a%2f%2faltex.ro%2fsistem-pc-gaming-myria-digital-22-intel-core-i5-8400-pana-la-4ghz-8gb-ssd-240gb-hdd-1tb-nvidia-geforce-gtx-1070-8gb-ubuntu%2fcpd%2fCLCMYRDIGITAL22";
+        str = 'https://' + str.split("https%3a%2f%2f").pop();
+        str = str.replace('%2f', '/');
+        console.log(str);
 
-    // const input = (document.getElementById('file') as HTMLInputElement);
-    // input.addEventListener('change', (event) => {
-    //   const files = input.files;
-    //   const len = files.length;
-
-    //   if (len) {
-    //     console.log('Filename: ' + files[0].name);
-    //     console.log('Type: ' + files[0].type);
-    //     console.log('Size: ' + files[0].size + ' bytes');
-
-    //   }
-
-    // }, false);
-    // this.data = this.readFile();
-    // console.log(this.data);
-
-
-    // const value = {
-    //   name: 'aaaaa',
-    //   description: 'Hub USB USB 3.1 Type-C HAMA Aluminium 135758, 2x USB-A, USB-C, 3.5 mm audio, argintiuPentru conectarea unui PC / notebook / MacBook / tableta cu USB-C la casti / boxe printr-o mufa jack de 3.5 mm (audio) si simultan conectarea a pana la 3 alte dispoz',
-    //   final_price: '109.99',
-    //   category: 'Laptop - Desktop - IT',
-    //   subcategory: 'Hub USB',
-    //   url: 'https://altex.ro%2fhub-usb-usb-3-1-type-c-hama-aluminium-135758-2x-usb-a-usb-c-3-5-mm-audio-argintiu%2fcpd%2fHUB135758',
-    //   url_img: 'https://cdna.altex.ro//resize/media/catalog/product/1/3/16fa6a9aef7ffd6209d5fd9338ffa0b1/135758_1_123e61ae.jpg',
-    //   SKU: 'HUB135758',
-    //   auto_generated: '',
-    //   brand: 'HAMA',
-    //   in_stock: 'da',
-    //   other: '',
-    //   original_price: '109.99',
-    //   promotion: 'nu'
-    // };
-    // const result = this.createProduct(value);
-    // console.log(result);
-
-  }
-
-  public changeListener(files: FileList) {
-    console.log(files);
-    if (files && files.length > 0) {
-      const file: File = files.item(0);
-      console.log(file.name);
-      console.log(file.size);
-      console.log(file.type);
-      const reader: FileReader = new FileReader();
-      reader.readAsText(file);
-      reader.onload = (e) => {
-        const txt: string = reader.result as string;
-        console.log(txt);
-
-        const csv = [];
-        const lines = txt.split('\n');
-        const csvSeparator = ';';
-        lines.forEach(element => {
-          const cols: string[] = element.split(csvSeparator);
-          csv.push(cols);
-        });
-        this.parsedCsv = csv;
-        console.log(this.parsedCsv);
-      };
     }
-  }
 
-  createProduct(value) {
-    console.log('value', value);
+    changeListener(files: FileList) {
+        console.log(files);
+        if (files && files.length > 0) {
+            const file: File = files.item(0);
+            console.log(file.name);
+            console.log(file.size);
+            console.log(file.type);
+            const reader: FileReader = new FileReader();
+            reader.readAsText(file);
+            reader.onload = (e) => {
+                const txt: string = reader.result as string;
+                console.log(txt);
 
-    return this.db.collection(new Date().toISOString().replace(/T.*/, '').split('-').reverse().join('-')).add({
-      name: value.name,
-      description: value.description,
-      brand: value.brand,
-      category: value.category,
-      subcategory: value.subcategory,
-      final_price: value.final_price,
-      in_stock: value.in_stock,
-      original_price: value.original_price,
-      url: value.url,
-      url_img: value.url_img,
-      SKU: value.SKU,
-      promotion: value.promotion,
-      other: value.other,
-      auto_generated: value.auto_generated
-    });
-  }
+                const csv = [];
+                const lines = txt.split('\n');
+                const csvSeparator = '","';
+                lines.forEach(element => {
+                    const cols: string[] = element.split(csvSeparator);
+                    csv.push(cols);
+                });
+                this.parsedCsv = csv;
+                this.parsedCsv.shift();
+                this.parsedCsv.pop();
+                console.log(this.parsedCsv);
+                let url: string;
+
+                this.parsedCsv.forEach((element, index) => {
+                    if (index < 100) {
+                        url = element[6];
+                        url = 'https://' + url.split("https%3a%2f%2f").pop();
+                        url = url.replace('%2f', '/');
+                        // console.log(url);
+                        
+                        this.product = {
+                            name: element[0],
+                            description: element[1],
+                            message: element[2],
+                            final_price: element[3],
+                            category: element[4],
+                            subcategory: element[5],
+                            url: url,
+                            url_img: element[7],
+                            SKU: element[8],
+                            auto_generated: element[9],
+                            brand: element[10],
+                            in_stock: element[11],
+                            other: element[12],
+                            original_price: element[13],
+                            promotion: element[14],
+                        }
+                        // this.productsCollection[index] = this.product;
+                        // this.productsCollection.push(this.product);
+                        
+                        this.createProduct(this.product);
+                    }
+                });
+                // console.log(this.productsCollection);
+                // const result = this.createProduct(this.productsCollection);
+                // console.log(result);
+            };
+        }
+    }
+
+    createProduct(value) {
+        console.log('value', value);
+
+        // return this.db.collection(value.SKU).doc(new Date().toISOString().replace(/T.*/, '').split('-').reverse().join('-')).set(value);
+
+        return this.db.collection(value.SKU).doc(new Date().toISOString().replace(/T.*/, '').split('-').reverse().join('-')).delete();
+
+        // return this.db.collection('products').doc(new Date().toISOString().replace(/T.*/, '').split('-').reverse().join('-')).set(value);
+    }
 
 }
